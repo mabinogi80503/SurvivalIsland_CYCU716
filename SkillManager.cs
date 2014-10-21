ea9898f150a981845ad8b441b92a0719e6e8e9ab
Warcat <love54132@gmail.com>
2014-10-21 22:34:15 +0800

    Test1

---
+++ SkillManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SkillManager : MonoBehaviour {
+    Dictionary<string, Skill> skillChain;
+    private GameObject mTarget;
+
+    public SkillManager(GameObject target) {
+        mTarget = target;
+        skillChain = new Dictionary<string,Skill>() ;
+        skillChain.Add("skill_1", new Skill());
+        GetSkill("skill_1");
+    }
+
+    public Skill GetSkill(string name)  {
+        if (skillChain.ContainsKey(name))
+            return skillChain[name];
+        else
+            return null;
+    } 
+}
